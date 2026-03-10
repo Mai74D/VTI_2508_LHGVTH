77430783ddb6bb52d33db4663bcf5922d98c578b
Mai74D <nguyenmaioc0@gmail.com>
2026-03-10 21:14:19 +0700

    VTI 2508: init project

---
+++ src/main/java/com/vti/vti2507_product_management/Vti2507ProductManagementApplication.java
@@ -0,0 +1,13 @@
+package com.vti.vti2507_product_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vti2507ProductManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vti2507ProductManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vti/vti2507_product_management/Vti2507ProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.vti2507_product_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vti2507ProductManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
